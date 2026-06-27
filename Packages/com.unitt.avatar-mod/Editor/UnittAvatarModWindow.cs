9422d35a04a669ad843d7425b488b916829fe76c
MistRal <parutegerenjianquyin@gmail.com>
2026-06-28 00:56:50 +0900

    Add wardrobe inventory UI

--- Packages/com.unitt.avatar-mod/Editor/UnittAvatarModWindow.cs
+++ Packages/com.unitt.avatar-mod/Editor/UnittAvatarModWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,6 +8,11 @@ namespace Unitt.AvatarMod.Editor
 {
     public sealed class UnittAvatarModWindow : EditorWindow
     {
+        private const float AvatarPreviewPanelWidth = 280f;
+        private const float AvatarPreviewSize = 220f;
+        private const float InventoryMinWidth = 320f;
+        private const float InventoryIconSize = 42f;
+
         private GameObject avatarRoot;
         private UnittModificationProfile modificationProfile;
         private GameObject accessoryPrefab;
@@ -14,27 +20,46 @@ namespace Unitt.AvatarMod.Editor
         private string attachBoneName = "Head";
         private bool applyMaterialToAllSkinnedMeshRenderers;
         private Vector2 scrollPosition;
+        private Vector2 inventoryScrollPosition;
+        private readonly List<GameObject> inventoryPrefabs = new List<GameObject>();
         private List<UnittValidationResult> validationResults = new List<UnittValidationResult>();
+        private GUIStyle panelStyle;
+        private GUIStyle playfulHeaderStyle;
+        private GUIStyle inventoryItemStyle;
+        private GUIStyle mutedLabelStyle;
 
         [MenuItem("Tools/Unitt!/Open")]
         public static void Open()
         {
             var window = GetWindow<UnittAvatarModWindow>();
             window.titleContent = new GUIContent(UnittLocalization.AppName);
-            window.minSize = new Vector2(420f, 360f);
+            window.minSize = new Vector2(720f, 460f);
             window.Show();
         }
 
         private void OnEnable()
         {
             titleContent = new GUIContent(UnittLocalization.AppName);
-            minSize = new Vector2(420f, 360f);
+            minSize = new Vector2(720f, 460f);
             UnittModularAvatarDependency.PromptInstallIfMissing();
 
             if (avatarRoot == null)
             {
                 avatarRoot = UnittAvatarDetector.DetectFromSelection();
             }
+
+            RefreshInventoryPrefabs();
+        }
+
+        private void OnFocus()
+        {
+            RefreshInventoryPrefabs();
+        }
+
+        private void OnProjectChange()
+        {
+            RefreshInventoryPrefabs();
+            Repaint();
         }
 
         private void OnSelectionChange()
@@ -48,16 +73,13 @@ namespace Unitt.AvatarMod.Editor
 
         private void OnGUI()
         {
+            InitializeStyles();
             titleContent = new GUIContent(UnittLocalization.AppName);
 
             scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
 
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField(UnittLocalization.AppName, EditorStyles.boldLabel);
-            EditorGUILayout.HelpBox(
-                "Drag an avatar, modification profile, accessory prefab, or replacement material here to apply an avatar modification.",
-                MessageType.Info);
-
+            DrawHeader();
+            DrawWardrobeLayout();
             DrawObjectFields();
             DrawActionButtons();
             DrawValidationResults();
@@ -65,68 +87,184 @@ namespace Unitt.AvatarMod.Editor
             EditorGUILayout.EndScrollView();
         }
 
+        private void DrawHeader()
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField(":: unitt! dress-up deck ::", playfulHeaderStyle);
+            EditorGUILayout.HelpBox(
+                "Pick an avatar on the left, then choose a loaded prefab from the Wardrobe Inventory on the right.",
+                MessageType.Info);
+        }
+
+        private void DrawWardrobeLayout()
+        {
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                DrawAvatarPreviewPanel();
+                DrawInventoryPanel();
+            }
+        }
+
+        private void DrawAvatarPreviewPanel()
+        {
+            using (new EditorGUILayout.VerticalScope(panelStyle, GUILayout.Width(AvatarPreviewPanelWidth)))
+            {
+                EditorGUILayout.LabelField("Avatar Preview", EditorStyles.boldLabel);
+                avatarRoot = (GameObject)EditorGUILayout.ObjectField(
+                    "Avatar Root",
+                    avatarRoot,
+                    typeof(GameObject),
+                    true);
+
+                var previewTexture = GetPreviewTexture(avatarRoot);
+                var previewRect = GUILayoutUtility.GetRect(
+                    AvatarPreviewSize,
+                    AvatarPreviewSize,
+                    GUILayout.Width(AvatarPreviewSize),
+                    GUILayout.Height(AvatarPreviewSize));
+
+                GUI.Box(previewRect, GUIContent.none);
+                if (previewTexture != null)
+                {
+                    EditorGUI.DrawPreviewTexture(previewRect, previewTexture, null, ScaleMode.ScaleToFit);
+                }
+                else
+                {
+                    GUI.Label(previewRect, "Drop an avatar here", EditorStyles.centeredGreyMiniLabel);
+                }
+
+                var selectedName = accessoryPrefab != null ? accessoryPrefab.name : "nothing yet";
+                EditorGUILayout.LabelField("Now wearing: " + selectedName, mutedLabelStyle);
+            }
+        }
+
+        private void DrawInventoryPanel()
+        {
+            using (new EditorGUILayout.VerticalScope(panelStyle, GUILayout.MinWidth(InventoryMinWidth)))
+            {
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    EditorGUILayout.LabelField("Wardrobe Inventory", EditorStyles.boldLabel);
+                    if (GUILayout.Button("Refresh", GUILayout.Width(72f)))
+                    {
+                        RefreshInventoryPrefabs();
+                    }
+                }
+
+                EditorGUILayout.LabelField(
+                    "Loaded project prefabs become playful outfit cards here.",
+                    mutedLabelStyle);
+
+                if (inventoryPrefabs.Count == 0)
+                {
+                    EditorGUILayout.HelpBox(
+                        "No prefabs found in the project yet. Add outfit prefabs to Assets, then press Refresh.",
+                        MessageType.Info);
+                    return;
+                }
+
+                inventoryScrollPosition = EditorGUILayout.BeginScrollView(
+                    inventoryScrollPosition,
+                    GUILayout.MinHeight(220f));
+
+                foreach (var prefab in inventoryPrefabs)
+                {
+                    DrawInventoryItem(prefab);
+                }
+
+                EditorGUILayout.EndScrollView();
+            }
+        }
+
+        private void DrawInventoryItem(GameObject prefab)
+        {
+            if (prefab == null)
+            {
+                return;
+            }
+
+            using (new EditorGUILayout.HorizontalScope(inventoryItemStyle))
+            {
+                var icon = AssetPreview.GetMiniThumbnail(prefab) as Texture2D;
+                GUILayout.Label(icon, GUILayout.Width(InventoryIconSize), GUILayout.Height(InventoryIconSize));
+
+                using (new EditorGUILayout.VerticalScope())
+                {
+                    EditorGUILayout.LabelField(prefab.name, EditorStyles.boldLabel);
+                    EditorGUILayout.LabelField(AssetDatabase.GetAssetPath(prefab), mutedLabelStyle);
+                }
+
+                using (new EditorGUILayout.VerticalScope(GUILayout.Width(92f)))
+                {
+                    if (GUILayout.Button("Set"))
+                    {
+                        accessoryPrefab = prefab;
+                        validationResults.Clear();
+                    }
+
+                    if (GUILayout.Button("Wear"))
+                    {
+                        WearInventoryPrefab(prefab);
+                    }
+                }
+            }
+        }
+
         private void DrawObjectFields()
         {
             EditorGUILayout.Space();
-
-            EditorGUI.BeginChangeCheck();
-            avatarRoot = (GameObject)EditorGUILayout.ObjectField(
-                "Avatar Root",
-                avatarRoot,
-                typeof(GameObject),
-                true);
-            if (EditorGUI.EndChangeCheck())
+            using (new EditorGUILayout.VerticalScope(panelStyle))
             {
-                validationResults.Clear();
-            }
+                EditorGUILayout.LabelField("Fitting Room Controls", EditorStyles.boldLabel);
 
-            EditorGUI.BeginChangeCheck();
-            modificationProfile = (UnittModificationProfile)EditorGUILayout.ObjectField(
-                "Modification Profile",
-                modificationProfile,
-                typeof(UnittModificationProfile),
-                false);
-            if (EditorGUI.EndChangeCheck())
-            {
-                ApplyProfile(modificationProfile);
-                validationResults.Clear();
-            }
-
-            EditorGUI.BeginChangeCheck();
-            accessoryPrefab = (GameObject)EditorGUILayout.ObjectField(
-                "Accessory Prefab",
-                accessoryPrefab,
-                typeof(GameObject),
-                false);
-            if (EditorGUI.EndChangeCheck())
-            {
-                validationResults.Clear();
-            }
-
-            EditorGUI.BeginChangeCheck();
-            replacementMaterial = (Material)EditorGUILayout.ObjectField(
-                "Replacement Material",
-                replacementMaterial,
-                typeof(Material),
-                false);
-            if (EditorGUI.EndChangeCheck())
-            {
-                if (replacementMaterial != null && modificationProfile == null)
+                EditorGUI.BeginChangeCheck();
+                modificationProfile = (UnittModificationProfile)EditorGUILayout.ObjectField(
+                    "Modification Profile",
+                    modificationProfile,
+                    typeof(UnittModificationProfile),
+                    false);
+                if (EditorGUI.EndChangeCheck())
                 {
-                    applyMaterialToAllSkinnedMeshRenderers = true;
+                    ApplyProfile(modificationProfile);
+                    validationResults.Clear();
                 }
 
-                validationResults.Clear();
-            }
+                EditorGUI.BeginChangeCheck();
+                accessoryPrefab = (GameObject)EditorGUILayout.ObjectField(
+                    "Accessory Prefab",
+                    accessoryPrefab,
+                    typeof(GameObject),
+                    false);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    validationResults.Clear();
+                }
 
-            EditorGUI.BeginChangeCheck();
-            attachBoneName = EditorGUILayout.TextField("Attach Bone Name", attachBoneName);
-            applyMaterialToAllSkinnedMeshRenderers = EditorGUILayout.ToggleLeft(
-                "Apply Material To All Skinned Mesh Renderers",
-                applyMaterialToAllSkinnedMeshRenderers);
-            if (EditorGUI.EndChangeCheck())
-            {
-                validationResults.Clear();
+                EditorGUI.BeginChangeCheck();
+                replacementMaterial = (Material)EditorGUILayout.ObjectField(
+                    "Replacement Material",
+                    replacementMaterial,
+                    typeof(Material),
+                    false);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    if (replacementMaterial != null && modificationProfile == null)
+                    {
+                        applyMaterialToAllSkinnedMeshRenderers = true;
+                    }
+
+                    validationResults.Clear();
+                }
+
+                EditorGUI.BeginChangeCheck();
+                attachBoneName = EditorGUILayout.TextField("Attach Bone Name", attachBoneName);
+                applyMaterialToAllSkinnedMeshRenderers = EditorGUILayout.ToggleLeft(
+                    "Apply Material To All Skinned Mesh Renderers",
+                    applyMaterialToAllSkinnedMeshRenderers);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    validationResults.Clear();
+                }
             }
         }
 
@@ -172,6 +310,31 @@ namespace Unitt.AvatarMod.Editor
             {
                 EditorGUILayout.HelpBox(result.Message, ToMessageType(result.Severity));
             }
+        }
+
+        private void RefreshInventoryPrefabs()
+        {
+            inventoryPrefabs.Clear();
+
+            var prefabGuids = AssetDatabase.FindAssets("t:Prefab");
+            foreach (var guid in prefabGuids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                if (prefab != null)
+                {
+                    inventoryPrefabs.Add(prefab);
+                }
+            }
+
+            inventoryPrefabs.Sort((left, right) =>
+                string.Compare(left.name, right.name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private void WearInventoryPrefab(GameObject prefab)
+        {
+            accessoryPrefab = prefab;
+            Apply();
         }
 
         private void ApplyProfile(UnittModificationProfile profile)
@@ -233,6 +396,47 @@ namespace Unitt.AvatarMod.Editor
             attachBoneName = "Head";
             applyMaterialToAllSkinnedMeshRenderers = false;
             validationResults.Clear();
+        }
+
+        private void InitializeStyles()
+        {
+            if (panelStyle != null)
+            {
+                return;
+            }
+
+            panelStyle = new GUIStyle(EditorStyles.helpBox)
+            {
+                padding = new RectOffset(10, 10, 8, 10),
+                margin = new RectOffset(4, 4, 4, 4)
+            };
+
+            playfulHeaderStyle = new GUIStyle(EditorStyles.boldLabel)
+            {
+                fontSize = 15,
+                alignment = TextAnchor.MiddleCenter
+            };
+
+            inventoryItemStyle = new GUIStyle(EditorStyles.helpBox)
+            {
+                padding = new RectOffset(6, 6, 6, 6),
+                margin = new RectOffset(2, 2, 3, 3)
+            };
+
+            mutedLabelStyle = new GUIStyle(EditorStyles.miniLabel)
+            {
+                wordWrap = true
+            };
+        }
+
+        private static Texture2D GetPreviewTexture(UnityEngine.Object target)
+        {
+            if (target == null)
+            {
+                return null;
+            }
+
+            return AssetPreview.GetAssetPreview(target) ?? (AssetPreview.GetMiniThumbnail(target) as Texture2D);
         }
 
         private static MessageType ToMessageType(UnittValidationSeverity severity)
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## 日本語
 
-Unitt-AvatarMod は、Unity 上で VRChat 向けアバター改変をドラッグ&ドロップで行うための Unity Editor 拡張です。
+Unitt-AvatarMod は、Unity 上で VRChat 向けアバター改変をドラッグ&ドロップで行うための Unity Editor 拡張です。現在の UI は、左側にアバターのプレビュー、右側に読み込まれている Prefab のインベントリを置いた、ちょっと遊び心のある着せ替え画面になっています。
 
 Unity Editor 内の表示名は、Editor の言語設定に応じて切り替わります。
 
@@ -27,20 +27,33 @@ https://unitt.github.io/Unitt-AvatarMod/vpm/index.json
 
 1. Unity でアバタープロジェクトを開きます。
 2. `Tools > Unitt! > Open` を選びます。
-3. `Avatar Root` に Scene 上のアバターをドラッグ&ドロップします。
-4. `Accessory Prefab` または `Replacement Material` をドラッグ&ドロップします。
-5. 必要に応じて `Attach Bone Name` を設定します。初期値は `Head` です。
-6. `Validate` でエラーや警告を確認します。
-7. `Dry Run` で変更予定を Console に出力します。
-8. `Apply` で実際に適用します。
+3. 左側の `Avatar Preview` に Scene 上のアバターを指定します。
+4. 右側の `Wardrobe Inventory` で、Project 内に読み込まれている Prefab を選びます。
+5. `Set` で `Accessory Prefab` にセットするか、`Wear` でそのまま装備として適用します。
+6. 必要に応じて `Attach Bone Name` を設定します。初期値は `Head` です。
+7. `Validate` でエラーや警告を確認します。
+8. `Dry Run` で変更予定を Console に出力します。
+9. `Apply` で実際に適用します。
+
+### UI の見方
+
+- 左側: アバタープレビューと現在選択中の装備名を表示します。
+- 右側: `AssetDatabase.FindAssets("t:Prefab")` で Project 内の Prefab を読み込み、Wardrobe Inventory として表示します。
+- `Refresh`: Project に追加した Prefab を再読み込みします。
+- `Set`: Prefab を選択状態にします。
+- `Wear`: 選択した Prefab を既存の Apply 処理へ渡して装備します。
 
 ### 補足
 
-- このプロジェクトはcodexを使用して作成されました。
+- このプロジェクトは Codex を使用して作成されました。
+- Accessory Prefab は指定 Bone 配下、または Avatar Root 直下に追加されます。
+- Material 差し替えは `SkinnedMeshRenderer` の全 slot を対象にできます。
+- 変更は Unity Undo に登録されます。
+- Modular Avatar が無い場合は、起動時にインストール案内を表示します。
 
 ## English
 
-Unitt-AvatarMod is a Unity Editor extension for applying simple VRChat avatar modifications through a drag-and-drop GUI. Drop an avatar, modification prefab, material, or saved profile into one IMGUI window, then validate, dry run, and apply the change with Undo support.
+Unitt-AvatarMod is a Unity Editor extension for applying simple VRChat avatar modifications through a drag-and-drop GUI. The current interface is a playful wardrobe view: an avatar preview on the left and a right-side inventory of loaded prefabs on the right.
 
 The in-Editor display name follows the Unity Editor language setting.
 
@@ -63,19 +76,27 @@ Then add `com.unitt.avatar-mod` to a Unity 2022.3 avatar project.
 
 1. Open an avatar project in Unity.
 2. Select `Tools > Unitt! > Open`.
-3. Drag a scene avatar into `Avatar Root`.
-4. Drag an `Accessory Prefab` or `Replacement Material` into the window.
-5. Set `Attach Bone Name` if needed. The default is `Head`.
-6. Press `Validate` to review errors and warnings.
-7. Press `Dry Run` to log the planned changes without modifying the scene.
-8. Press `Apply` to apply the modification.
+3. Assign a scene avatar in the left-side `Avatar Preview`.
+4. Pick a loaded project prefab from the right-side inventory, `Wardrobe Inventory`.
+5. Press `Set` to choose it as the `Accessory Prefab`, or press `Wear` to apply it immediately.
+6. Set `Attach Bone Name` if needed. The default is `Head`.
+7. Press `Validate` to review errors and warnings.
+8. Press `Dry Run` to log the planned changes without modifying the scene.
+9. Press `Apply` to apply the modification.
+
+### UI Notes
+
+- Left side: shows the avatar preview and currently selected outfit.
+- Right side: loads project prefabs with `AssetDatabase.FindAssets("t:Prefab")` and shows them as Wardrobe Inventory cards.
+- `Refresh`: reloads newly added project prefabs.
+- `Set`: selects a prefab.
+- `Wear`: sends the prefab through the existing Apply flow.
 
 ### Notes
 
-- Accessory prefabs are parented under the named bone when found, or under the avatar root as a fallback.
-- Material replacement targets all slots of every `SkinnedMeshRenderer` when material application is enabled.
+- Accessory prefabs are parented under the named bone when found, or under Avatar Root as a fallback.
+- Replacement materials can target every `SkinnedMeshRenderer` material slot.
 - Scene changes are registered with Unity Undo.
-- VRChat SDK types are detected by name instead of direct assembly references.
 - If Modular Avatar is missing, the window shows an installation prompt on startup.
 
 ## License
